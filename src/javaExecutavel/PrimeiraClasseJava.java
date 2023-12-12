0d34e0e42ee4e044cca0822a264a2a795d7739ec
diogo <diogogomes467@gmail.com>
2023-12-12 09:46:55 -0400

    aula de java

--- src/javaExecutavel/PrimeiraClasseJava.java
+++ src/javaExecutavel/PrimeiraClasseJava.java
@@ -3,6 +3,7 @@ package javaExecutavel;
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
-		System.out.println("Meu primeiro programa em java");		
+		System.out.println("Meu primeiro programa em java");
+		System.out.println("Meu primeiro programa em java");
 	}
 }
